3028ea1ee3d71bbad2dff4861c45dc85d174f18f
Marlène Charlet <italikdesignbordeaux@gmail.com>
2022-06-12 18:16:53 +0200

    Update ArduinoCalculator.ino

--- ArduinoCalculator.ino
+++ ArduinoCalculator.ino
@@ -1,13 +1,13 @@
+// C++ code
+//
+
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 int number1 = 0;
 int number2 = 0;
 int resultat = 0;
-int typeResult;
-int result1 = 0;
-int result2 = 0;
-int result3 = 0;
+
 
 
 
@@ -21,6 +21,12 @@ void setup() {
  lcd.setCursor(0, 1);
  lcd.print("Calculator !");
   
+  /* Application randomSeed sur une broche analogique vide
+  pour que les nombres soient réellement aléatoires
+  (le random est aléatoire mais toujours avec les mêmes nombres
+  au démarrage du programme)*/
+  randomSeed(A0);
+  
   //Effacer l'écran avant de passer à une nouvelle opération  
   delay(5000);
   lcd.clear();
@@ -29,88 +35,51 @@ void setup() {
 }
 
 void loop() {
-  
-
 
 //Génération des nombres aléatoires de 0 à 20
 number1 = random(0, 20);
-
-  
 number2 = random(0, 20);
 
-
-//Affichage de l'opération et calcul du résultat
+resultat = number1 + number2;
+  
+//affichage de la question
 lcd.print(number1);
 lcd.print(" + ");
 lcd.print(number2);
 lcd.print(" = ");
-resultat = number1 + number2;
-Serial.println(resultat);
   
+//création d'un tableau pour les 3 réponses possibles
+  int tab[] = {resultat+random(1, 3), resultat-random(1,3),resultat};
+  const byte N = sizeof(tab)/sizeof(tab[0]);
+  byte i=0;
 
+//melange des 3 réponses dans le tableau
+    for(i=0; i< N-1; i++) {
+      byte j = random(i, N);
+      int echange = tab[i];
+      tab[i] = tab[j];
+      tab[j] = echange;
+    }
+    //Serial.println(tab[i]);
   
-//Affichage des résultats (dont 2 faux)
+//affichage des réponses
 lcd.setCursor(0,1);
 lcd.print("a)");
-typeResult = random(2);
-//Définir type de résultat
-  switch(typeResult){
-    case 0 :
-    result1 = resultat + random(1, 6);
-    lcd.print(result1);
-    break;
-    case 1 :
-    result1 = resultat;
-    lcd.print(result1);
-    break;
-  }
-
-
+lcd.print(tab[0]);
   
-lcd.setCursor(6, 1);
+lcd.setCursor(6,1);
 lcd.print("b)");
-  if(result1 == resultat){
-    result2 = resultat + random(1, 6);
-    if(result2 == result1) {
-    lcd.print(result2 + random(1, 2));
-    }
-    else {
-    lcd.print(result2);
-    }
-   }
-   else {
-    result2 = resultat;
-    lcd.print(result2);
-  }
+lcd.print(tab[1]);
   
-  
-   
-  
-  
-
-
-  
-  
-lcd.setCursor(12, 1);
+lcd.setCursor(12,1);
 lcd.print("c)");
-  if(result1 != resultat && result2 != resultat) {
-    result3 = resultat;
-    lcd.print(result3);
-  }
-  else {
-    result3 = resultat + random(1, 6);
-    if(result3 == result1 || result3 == result2){
-    result3 = result3 + random(1, 6);
-    }
-    lcd.print(result3);
-    
-  }
+lcd.print(tab[2]);
+  
  
- 
-  //Effacer l'écran avant de passer à une nouvelle opération  
-  delay(10000);
-  lcd.clear();
-  lcd.setCursor(0,0);
+//Effacer l'écran avant de passer à une nouvelle opération  
+delay(10000);
+lcd.clear();
+lcd.setCursor(0,0);
   
 
  
